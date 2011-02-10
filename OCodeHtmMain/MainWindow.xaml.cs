deb02af4b89be620dae6d64df7ccbdec4d67782a
neural5torm <neuralstorm5@gmail.com>
2011-02-10 18:19:45 +0100

    First two projects (lib and demo app)

---
+++ OCodeHtmMain/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OCodeHtmMain
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string TITLE = "OCodeHTM Demo";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title = TITLE;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Feature not yet implemented!", TITLE, MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+    }
+}
